5b01fce1d4739290bd52ddeab01c6b18f8d03b63
Alex <a.cooparr@gmail.com>
2026-08-19 19:22:27 +0100

    Initial Commit

---
+++ PhotoTinder/PhotoTinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoTinderApp.swift
+//  PhotoTinder
+//
+//  Created by Alexander Cooper on 19/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct PhotoTinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
